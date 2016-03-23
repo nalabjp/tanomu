e931c66e5cb262afdb2554734faa061b9425a20b
Naoto Koshikawa <koshikawa@ppworks.jp>
2016-03-23 22:03:50 +0900

    Refactor

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -6,10 +6,16 @@ class Webhook
     def run(event_type: , payload:)
       return if event_type.nil?
 
-      event = Github.const_get event_type.split(/_/).map(&:capitalize).join
+      event = event_class(event_type)
       event.hook(payload: payload)
     rescue NameError => e
       raise unless e.message =~ /uninitialized constant/
+    end
+
+    private
+
+    def event_class(event_type)
+      Github.const_get event_type.split(/_/).map(&:capitalize).join
     end
   end
 end
