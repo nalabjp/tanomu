ce15f4abf4ecb4c36865f8efae7ea42f8ae9c612
Naoto Koshikawa <koshikawa@ppworks.jp>
2016-03-23 22:40:01 +0900

    Comment for Github event

--- app/models/github/issue_comment.rb
+++ app/models/github/issue_comment.rb
@@ -2,6 +2,17 @@ module Github
   class IssueComment
     class << self
       def hook(payload:)
+        return if payload.nil?
+
+        comment = payload.dig('comment', 'body')
+
+        # TODO
+        # commentからteamパターンを取得
+        # team名を取得
+        # team_idを取得
+        # membersを取得
+        # randomに選出
+        # prに対してassign
       end
     end
   end
--- app/models/github/pull_request.rb
+++ app/models/github/pull_request.rb
@@ -2,6 +2,18 @@ module Github
   class PullRequest
     class << self
       def hook(payload:)
+        return if payload.nil?
+
+        assignee = payload.dig('pull_request', 'assignee', 'login')
+        return if assignee.nil?
+
+        # TODO
+        # repository名を取得
+        # team名を取得
+        # team_idを取得
+        # membersを取得
+        # randomに選出
+        # prに対してassign
       end
     end
   end
