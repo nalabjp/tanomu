8033cc72c4a1e681bee96fef9f948977a90be5da
ppworks <koshikawa@ppworks.jp>
2016-03-24 15:09:53 +0900

    Revert "Webhook secret"
    This reverts commit ce25d116c4edd37e6fa7323006275b699737ab82.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 ruby '2.3.0'
 source 'https://rubygems.org'
 
-gem 'rack-github_webhooks'
 gem 'sinatra'
 gem 'rake'
 gem 'octokit'
--- app.rb
+++ app.rb
@@ -1,12 +1,9 @@
 require 'json'
 require 'sinatra/base'
-require 'rack/github_webhooks'
 require './app/models/webhook'
 
 class App < Sinatra::Base
-  use Rack::GithubWebhooks, secret: ENV['SECRET'] if ENV['SECRET']
-
-  post '/' do
+  post "/#{ENV['TOKEN']}" do
     Webhook.run(event_type: request.env['HTTP_X_GITHUB_EVENT'], payload: payload)
     [204]
   end
