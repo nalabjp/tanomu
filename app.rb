65b66c3442dfd6aa810c7b02d9051aa7f3f8e026
ppworks <koshikawa@ppworks.jp>
2016-03-24 14:53:28 +0900

    fix test

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'rack/github_webhooks'
 require './app/models/webhook'
 
 class App < Sinatra::Base
-  use Rack::GithubWebhooks, secret: ENV['SECRET'] if ENV['SECRET'].nil?
+  use Rack::GithubWebhooks, secret: ENV['SECRET'] if ENV['SECRET']
 
   post '/' do
     Webhook.run(event_type: request.env['HTTP_X_GITHUB_EVENT'], payload: payload)
